ae52c9cbd1055ea7fb43e04a4ab6e6582dc86b16
Chandler Huff <contact@bludesign.biz>
2026-07-25 07:56:37 -0400

    ai-development-environment 0.0.51
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/ai-development-environment.rb
+++ Formula/ai-development-environment.rb
@@ -1,8 +1,8 @@
 class AiDevelopmentEnvironment < Formula
   desc "AI-focused development environment"
   homepage "https://github.com/bludesign/ai-development-environment"
-  url "https://github.com/bludesign/ai-development-environment/archive/refs/tags/v0.0.50.tar.gz"
-  sha256 "c115a1563db71ea584589da16753a30f7bafb8dc72632f1d29cdb6c34be333c3"
+  url "https://github.com/bludesign/ai-development-environment/archive/refs/tags/v0.0.51.tar.gz"
+  sha256 "4ae2d12fb4fc2ac1cd457a435896d3e62024a0cc4de304bb7bdaad8103900cf4"
 
   depends_on "node@24"
 
